d2339f83905987aea2a26f32eac644a0d96fd3e7
weihsuehl <weihsuehl@yahooinc.com>
2023-03-18 23:37:00 +0800

    lower speed for XY probe

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1484,14 +1484,14 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -44, -14.4, -0.4 }
+#define NOZZLE_TO_PROBE_OFFSET { -44, -14.4, -0.75 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
 #define PROBING_MARGIN 30
 
 // X and Y axis travel speed (mm/min) between probes
-#define XY_PROBE_FEEDRATE (150*60)
+#define XY_PROBE_FEEDRATE (133*60)
 
 // Feedrate (mm/min) for the first approach when double-probing (MULTIPLE_PROBING == 2)
 #define Z_PROBE_FEEDRATE_FAST (20*60)
